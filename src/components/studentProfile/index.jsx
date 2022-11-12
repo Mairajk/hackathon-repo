d154194d87957462c16403ba1b361472c8407e3b
Mairajk <mairajkhan597@gmail.com>
2022-11-12 17:39:05 +0500

    working.....4

--- src/components/studentProfile/index.jsx
+++ src/components/studentProfile/index.jsx
@@ -2,9 +2,22 @@
 
 
 const StudentProfile = () => {
+
+    const addingStudent = () => {
+        console.log("addingStudent");
+    }
+
+    const searchingStudent = () => {
+        console.log("searchingStudent");
+    }
+ 
     return(
         <div>
-            <h1> Student Profile Page</h1>
+            <h1> Student Profile Page 7</h1>
+
+            <button onClick={addingStudent}> Add new student </button>
+            <button onClick={searchingStudent}> Search Student</button>
+
         </div>
     )
 }
